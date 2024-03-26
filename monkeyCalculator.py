1049176b77dda8295a030704813924bb493f0a8f
Marco Potes <potesmarco@yahoo.com>
2024-03-25 23:50:16 -0500

    first attempt

--- monkeyCalculator.py
+++ monkeyCalculator.py
@@ -1,8 +1,19 @@
 def calculateTime():
     
     # This first line is provided for you
-    monkey_one = input("Is the first monkey smiling?:  ")
-    monkey_two = input("Is the second monkey smiling?: ")
+    monkey_one = input("Is the first monkey smiling?y/n:  ")
+    monkey_two = input("Is the second monkey smiling?y/n: ")
+
+    if monkey_one == "y" and monkey_two == "y":
+        print("Uh Oh! We're in trouble!")
+    elif monkey_one == "n" and monkey_two == "n":
+        print("Uh Oh! We're in trouble!")
+    elif monkey_one == 'y' and monkey_two =='n':
+        print("Yay! We're going to have a good day!")
+    elif monkey_one == 'n' and monkey_two =='y':
+        print("Yay! We're going to have a good day!")
+
+
 
     # end assignment
 
